6c55166f406f62c8a5576eb6ad4e807b73180bbe
MostafaKolkela <115589891+MostafaKolkela@users.noreply.github.com>
2024-09-28 22:31:06 +0300

    v.0

---
+++ model/userModel.js
@@ -0,0 +1,39 @@
+const mongoose = require('mongoose')
+const validator = require('validator')
+const userRoles = require('../utils/userRoles')
+
+const userSchema = new mongoose.Schema({
+    firstName:{
+        type:String,
+        required:true
+    },
+    lastName:{
+        type:String,
+        required:true
+    },
+    email:{
+        type:String,
+        required:true,
+        unique:true,
+        validate:[validator.isEmail,'field must be an email']
+    },
+    password:{
+        type:String,
+        required:true,
+    },
+    phoneNumber:{},
+    role:{
+        type:String,
+        enum:[userRoles.ADMIN,userRoles.USER],
+        default:userRoles.USER
+    },
+    token:{
+        type:String
+    },
+    avatar:{
+        type:String,
+        default:'uploads/defaulfAvatar.jpg'
+    }
+})
+
+module.exports= mongoose.model('User',userSchema)
